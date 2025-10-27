642f5d2657ed46d6cc18d01a7f2e212fd1621447
TheCodeMage <mr.phyoekyawkyaw@gmail.com>
2025-10-27 12:15:30 +0630

    5th Commit

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone # (to fix timezone error)
 
-# Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phone = models.IntegerField(default=0) # 9999999999 ထားခဲ့လည်း ရတယ်
+    joined_date = models.DateField(default=timezone.now)
+
